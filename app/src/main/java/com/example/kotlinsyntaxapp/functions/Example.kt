67d6c44b6e34801290ae4a0216cd6ec88df69ecb
Mahsa Azizy <37101768+azizymahsa@users.noreply.github.com>
2025-01-15 19:14:12 -0800

    Update Example.kt
    function

--- app/src/main/java/com/example/kotlinsyntaxapp/functions/Example.kt
+++ app/src/main/java/com/example/kotlinsyntaxapp/functions/Example.kt
@@ -5,3 +5,11 @@ package com.example.kotlinsyntaxapp.functions
 fun example() {
     println("This is an example of functions")
 }
+//Basic Function
+fun greet(name: String) {
+return "Hello, $name!"
+}
+fun main() {
+val message = greet("Kotlin")
+println(message)
+}
